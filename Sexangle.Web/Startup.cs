fec8faca7cacbd6698a7ae343f9e68070ba53e95
Anton Lukyanov <a.lukyanov@itransition.com>
2018-01-14 17:40:31 +0300

    Configured RootDirectory for razor pages + fixed layout

--- Sexangle.Web/Startup.cs
+++ Sexangle.Web/Startup.cs
@@ -21,7 +21,12 @@ namespace Sexangle.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services
+                .AddMvc()
+                .AddRazorPagesOptions(options =>
+                    {
+                        options.RootDirectory = "/Pages";
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
